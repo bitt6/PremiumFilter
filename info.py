30491dbb281be3b1ca8ec7bd8064681cc4ca09e9
FOZIA QURAISHI <103851865+bitt6@users.noreply.github.com>
2024-02-01 23:50:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,8 +69,8 @@ AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 1000))
 
 FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', LOG_CHANNEL))
 
-SHORTNER_SITE =  environ.get('SHORTNER_SITE', '') #Put Only Shortner Site domain don't put like this https://tnlink.in/
+SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'instantlinks.in') #Put Only Shortner Site domain don't put like this https://tnlink.in/
 
-SHORTNER_API =  environ.get('SHORTNER_API', '')
+SHORTNER_API =  environ.get('SHORTNER_API', '21bad4e94a15dcbfc685f8bf5a6d37a7da13df15')
 
 AUTO_DELETE =  environ.get('AUTO_DELETE', 'True')
